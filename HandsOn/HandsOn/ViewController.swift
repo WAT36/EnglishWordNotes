8d158a190f774b93a835578fc230135883ac10bf
T.Wakasugi <>
2019-01-05 15:53:08 +0900

    [iPhone] UILabel をコードで記述し画面回転時の位置調整をする

--- HandsOn/HandsOn/ViewController.swift
+++ HandsOn/HandsOn/ViewController.swift
@@ -10,15 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //ラベルのインスタンス生成
+    let testLabel = UILabel()
+    
     @IBOutlet var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setLabel()
         
         label.text = "Hello World!"
     }
 
-
+    //回転を検知
+    override func viewDidAppear(_ animated: Bool) {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.rotationChange(notification:)), name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+    
+    @objc
+    func rotationChange(notification: NSNotification){
+        setLabel()
+    }
+    
+    func setLabel(){
+        
+        let screenWidth:CGFloat = view.frame.size.width
+        let screenHeight:CGFloat = view.frame.size.height
+        
+        //ラベルのサイズを設定
+        testLabel.frame = CGRect(x:0, y:screenHeight-80, width:screenWidth, height:80);
+        
+        //ラベルの文字を設定
+        testLabel.text = "(testLabel)Hello,World!"
+        
+        //ラベルの色を黒に設定
+        testLabel.textColor = UIColor.black
+        
+        //ラベルのフォントを設定
+        testLabel.font = UIFont.boldSystemFont(ofSize: 30)
+        
+        //わかりやすくするため背景色を設定
+        testLabel.backgroundColor = UIColor(red:0.7, green:1.0, blue:0.9, alpha:1.0)
+        
+        //テキスト中央寄せ
+        testLabel.textAlignment = NSTextAlignment.center
+        
+        //Viewにラベルを追加
+        self.view.addSubview(testLabel)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated
+    }
 }
 
