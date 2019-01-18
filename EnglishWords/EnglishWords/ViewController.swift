6963a3b7934495d7bfd0321a7368d872b98bb025
T.Wakasugi <>
2019-01-18 23:51:14 +0900

    ハンズオンを参考にTableViewのサンプルを作成。

--- EnglishWords/EnglishWords/ViewController.swift
+++ EnglishWords/EnglishWords/ViewController.swift
@@ -2,26 +2,47 @@
 //  ViewController.swift
 //  EnglishWords
 //
-//  Created by Wataru Tsukagoshi on 2018/09/16.
-//  Copyright © 2018年 Wataru Tsukagoshi. All rights reserved.
+//  Created by T.Wakasugi on 2018/09/16.
+//  Copyright © 2018年 T.Wakasugi. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var table:UITableView!
     
-    @IBOutlet var label: UILabel!
+    //
+    let TODO = ["aiueo","kakikukeko","sasisuseso"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        label.text = "Hello World!"
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    //Table Viewのセルの数を指定
+    func tableView(_ table: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return TODO.count
+    }
+    
+    //各セルの要素を設定する
+    func tableView(_ table: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // tableCell の ID で UITableViewCell のインスタンスを生成
+        let cell = table.dequeueReusableCell(withIdentifier: "tablecell",
+                                             for: indexPath)
+        
+        // Tag番号  で UILabel インスタンスの生成
+        let label1 = cell.viewWithTag(1) as! UILabel
+        label1.text = TODO[indexPath.row]
+        
+        return cell
+    }
 }
 
