1bebfc04c764865dc2ce10b135e073b7aaaa631c
T.Wakasugi <>
2019-01-26 17:55:05 +0900

    トップ画面のテーブルを１つ増やした（オプション等用）

--- EnglishWords/EnglishWords/ViewController.swift
+++ EnglishWords/EnglishWords/ViewController.swift
@@ -12,9 +12,12 @@ import RealmSwift
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet var table:UITableView!
+    @IBOutlet var sidetable:UITableView!
     
     var booknamelist: [WordNoteBook] = []
     var wordnotebook: WordNoteBook?
+    
+    let sidebarlist = ["単語帳追加","品詞追加","マスター英単語帳","オプション"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         booknamelist = Array(results)
         booknamelist.append(WordNoteBook(value: ["wordNoteBookId": 999,
                                                  "wordNoteBookName": "単語帳を追加"]))
+        
+        //sidetableのラベルを折り返す設定
+        sidetable.estimatedRowHeight=120
+        sidetable.rowHeight=UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,36 +41,57 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //Table Viewのセルの数を指定
     func tableView(_ table: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        return booknamelist.count
+        
+        if table.tag == 1 {
+            print(sidebarlist.count)
+            return sidebarlist.count
+        }else{
+            print(booknamelist.count)
+            return booknamelist.count
+        }
     }
     
     //各セルの要素を設定する
     func tableView(_ table: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        // tableCell の ID で UITableViewCell のインスタンスを生成
-        let cell = table.dequeueReusableCell(withIdentifier: "tablecell",
+        print(table.tag)
+        if table.tag == 1 {
+            // tableCell の ID で UITableViewCell のインスタンスを生成
+            let cell = table.dequeueReusableCell(withIdentifier: "sidetablecell",
+                                                 for: indexPath)
+            // Tag番号  で UILabel インスタンスの生成
+            let label1 = cell.viewWithTag(1) as! UILabel
+            label1.numberOfLines = 0
+            label1.text = sidebarlist[indexPath.row]
+            
+            return cell
+        }else{
+            // tableCell の ID で UITableViewCell のインスタンスを生成
+            let cell = table.dequeueReusableCell(withIdentifier: "tablecell",
                                              for: indexPath)
-        
-        // Tag番号  で UILabel インスタンスの生成
-        let booknames = booknamelist[indexPath.row]
-        let label1 = cell.viewWithTag(1) as! UILabel
-        label1.text = booknames.wordNoteBookName
+            // Tag番号  で UILabel インスタンスの生成
+            let booknames = booknamelist[indexPath.row]
+            let label1 = cell.viewWithTag(1) as! UILabel
+            label1.numberOfLines = 0
+            label1.text = booknames.wordNoteBookName
 
-        return cell
+            return cell
+        }
     }
     
     // Cell が選択された場合
     func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
-        let selectedText = booknamelist[indexPath.row].wordNoteBookName
-        if selectedText == "単語帳を追加" {
-            // AddWordNoteBookViewController へ遷移するために Segue を呼び出す
-            performSegue(withIdentifier: "toSubViewController",sender: nil)
-        }else{
-            //選択したセルの単語帳を記録
-            wordnotebook = booknamelist[indexPath.row]
-            // ConfigureWordNoteBookViewController へ遷移するために Segue を呼び出す
-            performSegue(withIdentifier: "toConfigureWordNoteBookViewController", sender: nil)
+        if table.tag == 0 {
+            let selectedText = booknamelist[indexPath.row].wordNoteBookName
+            if selectedText == "単語帳を追加" {
+                // AddWordNoteBookViewController へ遷移するために Segue を呼び出す
+                performSegue(withIdentifier: "toSubViewController",sender: nil)
+            }else{
+                //選択したセルの単語帳を記録
+                wordnotebook = booknamelist[indexPath.row]
+                // ConfigureWordNoteBookViewController へ遷移するために Segue を呼び出す
+                performSegue(withIdentifier: "toConfigureWordNoteBookViewController", sender: nil)
+            }
         }
     }
     
@@ -93,6 +121,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             try! realm.write {
                 realm.delete(wordnotebook!)
             }
+        }
+    }
+    
+    // sideTableのCell の高さを１２０にする
+    func tableView(_ table: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if table.tag == 1 {
+            return 120.0
+        }else{
+            return 60.0
         }
     }
 }
