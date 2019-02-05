ce665ba0ee3a36417283c155f8575eb5df923d91
T.Wakasugi <>
2019-02-05 21:30:19 +0900

    単語設定画面に単語の意味が出るように実装

--- EnglishWords/EnglishWords/ConfigureWordViewController.swift
+++ EnglishWords/EnglishWords/ConfigureWordViewController.swift
@@ -10,23 +10,58 @@ import Foundation
 import UIKit
 import RealmSwift
 
-class ConfigureWordViewController: UIViewController{
+class ConfigureWordViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var wordnamelabel: UILabel!
     @IBOutlet var table:UITableView!
     
     var wordnotebook: WordNoteBook?
+    var worddatalist: [WordData] = []
     var wordnote: WordNote?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        wordnamelabel.text = wordnote?.worddata?.word?.wordName
+        let wordname = wordnote?.worddata?.word?.wordName
+        wordnamelabel.text = wordname
         
+        //選択したWordDataからデータベース内に保存してあるWordDataを全て取得
+        let realm: Realm = try! Realm()
+        let results = realm.objects(WordData.self).filter("word.wordName == %@",wordname!)
+        worddatalist = Array(results)
+        
+        //tableのラベルを折り返す設定
+        table.estimatedRowHeight=120
+        table.rowHeight=UITableViewAutomaticDimension
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    //Table Viewのセルの数を指定
+    func tableView(_ table: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return worddatalist.count
+    }
+    
+    //各セルの要素を設定する
+    func tableView(_ table: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        // tableCell の ID で UITableViewCell のインスタンスを生成
+        let cell = table.dequeueReusableCell(withIdentifier: "tablecell",
+                                                 for: indexPath)
+        // Tag番号  で UILabel インスタンスの生成
+        let oneworddata = worddatalist[indexPath.row]
+        let partofspeech = cell.viewWithTag(1) as! UILabel
+        partofspeech.numberOfLines = 0
+        partofspeech.text = oneworddata.partofspeech?.partsOfSpeechName
+        let mean = cell.viewWithTag(2) as! UILabel
+        mean.numberOfLines = 0
+        mean.text = oneworddata.mean
+        
+        return cell
     }
     
     @IBAction func buttonTapped(sender : AnyObject) {
@@ -41,4 +76,9 @@ class ConfigureWordViewController: UIViewController{
         }
     }
     
+    // sideTableのCell の高さを１２０にする
+    func tableView(_ table: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120.0
+    }
 }
