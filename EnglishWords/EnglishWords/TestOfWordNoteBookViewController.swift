aa94c81ff975de3231adf82b236a03478c9ff0fa
T.Wakasugi <motohari.xanadu@gmail.com>
2019-05-24 00:03:18 +0900

    単語テスト画面のテーブルに訳文表示・次の単語ボタンクリックで次の単語表示

--- EnglishWords/EnglishWords/TestOfWordNoteBookViewController.swift
+++ EnglishWords/EnglishWords/TestOfWordNoteBookViewController.swift
@@ -10,15 +10,18 @@ import Foundation
 import UIKit
 import RealmSwift
 
-class TestOfWordNoteBookViewController: UIViewController {
+class TestOfWordNoteBookViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     
-    @IBOutlet var nowWord: UILabel!
-    @IBOutlet var nowWordNote: UILabel!
+    @IBOutlet var word: UILabel!
+    @IBOutlet var wordNote: UILabel!
     @IBOutlet var count: UILabel!
     
     @IBOutlet var table:UITableView!
 
+    var nowWord: Word?
+    var wordIdx: Int = 0
     var wordnotebook: WordNoteBook?
+    var nowWordDataList: [WordData] = []
     
     //検索条件のリスト
     var queryList: [String] = []
@@ -41,9 +44,18 @@ class TestOfWordNoteBookViewController: UIViewController {
             //検索結果無し、エラーアラート出して戻させる
             showAlert(mes: "検索結果がありません")
         }else{
-            nowWord.text = wordNoteList[0].word?.wordName
-            nowWordNote.text = wordnotebook?.wordNoteBookName
-            count.text = "1/" + wordNoteList.count.description
+            wordIdx = 0
+            nowWord = wordNoteList[wordIdx].word
+
+            word.text = wordNoteList[wordIdx].word?.wordName
+            wordNote.text = wordnotebook?.wordNoteBookName
+            count.text = wordIdx.description + "/" + wordNoteList.count.description
+
+            //選択したWordからデータベース内に保存してあるWordDataを全て取得
+            let realm: Realm = try! Realm()
+            let results = realm.objects(WordData.self).filter("word.wordName == %@",nowWord?.wordName)
+            nowWordDataList = Array(results)
+
         }
     }
     
@@ -54,6 +66,9 @@ class TestOfWordNoteBookViewController: UIViewController {
     @IBAction func buttonTapped(sender: UIButton) {
         if(sender.tag == 0){
             performSegue(withIdentifier: "returntoConfigureTestOfWordNoteBookViewController",sender: nil)
+        }else if(sender.tag == 1){
+            //次の単語へ
+            toNextWord()
         }
     }
     
@@ -63,6 +78,59 @@ class TestOfWordNoteBookViewController: UIViewController {
             let ctwnbVC: ConfigureTestOfWordNoteBookViewController = (segue.destination as? ConfigureTestOfWordNoteBookViewController)!
             ctwnbVC.wordnotebook = wordnotebook
         }
+    }
+    
+    //Table Viewのセルの数を指定
+    func tableView(_ table: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return nowWordDataList.count
+    }
+    
+    //各セルの要素を設定する
+    func tableView(_ table: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // tableCell の ID で UITableViewCell のインスタンスを生成
+        let cell = table.dequeueReusableCell(withIdentifier: "tablecell",
+                                             for: indexPath)
+        // Tag番号  で UILabel インスタンスの生成
+        let oneworddata = nowWordDataList[indexPath.row]
+        let partofspeech = cell.viewWithTag(1) as! UILabel
+        partofspeech.numberOfLines = 0
+        partofspeech.text = oneworddata.partofspeech?.partsOfSpeechName
+        let mean = cell.viewWithTag(2) as! UILabel
+        mean.numberOfLines = 0
+        mean.text = oneworddata.mean
+        cell.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 1.0, alpha: 0.5)
+        
+        return cell
+    }
+    
+    //次の単語へ移る
+    func toNextWord(){
+        if(wordIdx >= wordNoteList.count - 1){
+            //テスト終了
+        }else{
+            wordIdx += 1
+            nowWord = wordNoteList[wordIdx].word
+            
+            word.text = wordNoteList[wordIdx].word?.wordName
+            wordNote.text = wordnotebook?.wordNoteBookName
+            count.text = wordIdx.description + "/" + wordNoteList.count.description
+            
+            //選択したWordからデータベース内に保存してあるWordDataを全て取得
+            let realm: Realm = try! Realm()
+            let results = realm.objects(WordData.self).filter("word.wordName == %@",nowWord?.wordName)
+            nowWordDataList = Array(results)
+            
+            //テーブル更新
+            table.reloadData()
+        }
+    }
+    
+    // tableのCell の高さを１２０にする
+    func tableView(_ table: UITableView,
+                   heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 90.0
     }
     
     //アラートを出すメソッド
