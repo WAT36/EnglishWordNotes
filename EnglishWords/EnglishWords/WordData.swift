4351be781b713746b31647fdfa5e191a47e361d3
T.Wakasugi <motohari.xanadu@gmail.com>
2019-06-26 08:17:19 +0900

    Realm定義変更、Wordに発音記号とレベルを別わけ、WordDataに例文英日を追加

--- EnglishWords/EnglishWords/WordData.swift
+++ EnglishWords/EnglishWords/WordData.swift
@@ -14,8 +14,9 @@ class WordData: Object{
     @objc dynamic var partofspeech: PartsofSpeech?
     @objc dynamic var meanidx: Int = -1
     @objc dynamic var mean = " "
-    @objc dynamic var example = " "
-    let source = List<Source>()     //出典のリスト
+    @objc dynamic var example_q = " "   //例文(問)
+    @objc dynamic var example_a = " "   //例文(答)
+    let source = List<Source>()         //出典のリスト
 
     
     static func ==(a: WordData, b: WordData) -> Bool{
