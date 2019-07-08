0f55e75f5f5b3e6367cd4d0df62aee30b0960526
T.Wakasugi <motohari.xanadu@gmail.com>
2019-07-09 00:17:14 +0900

    とりあえず四択単語テスト画面を追加　中身はまだ未実装

---
+++ EnglishWords/EnglishWords/FourOptionTestOfWordNoteViewController.swift
@@ -0,0 +1,27 @@
+//
+//  FourOptionTestOfWordNoteViewController.swift
+//  EnglishWords
+//
+//  Created by T.Wakasugi on 2019/07/09.
+//  Copyright © 2019年 T.Wakasugi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FourOptionTestOfWordNoteViewController: UIViewController  {
+    
+    @IBOutlet var word: UILabel!
+    @IBOutlet var wordNote: UILabel!
+    @IBOutlet var count: UILabel!
+    @IBOutlet var clearLabel: UILabel!
+    @IBOutlet var rate: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
